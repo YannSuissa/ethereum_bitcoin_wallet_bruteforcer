cdc65e011ead7b100c999a564c686c47e9847e98
Yann Suissa <yann.suissa@gmail.com>
2023-03-29 22:28:11 +0200

    Create wins.sh

---
+++ wins.sh
@@ -0,0 +1,5 @@
+ssh -n -p 9422 root@boxidm.r3factory.io  "echo --------------- ; cat ~/cpp/ethereum_bitcoin_wallet_bruteforcer/win.txt ; echo --------------- ; "  &
+ssh -n -p 9422 root@boxbla.r3factory.io  "echo --------------- ; cat ~/cpp/ethereum_bitcoin_wallet_bruteforcer/win.txt ; echo --------------- ; "  &
+ssh -n -p 9422 root@boxbla1.r3factory.io "echo --------------- ; cat ~/cpp/ethereum_bitcoin_wallet_bruteforcer/win.txt ; echo --------------- ; "  &
+ssh -n -p 9422 root@boxbla2.r3factory.io "echo --------------- ; cat ~/cpp/ethereum_bitcoin_wallet_bruteforcer/win.txt ; echo --------------- ; "  &
+ssh -n -p 9422 root@boxbla3.r3factory.io "echo --------------- ; cat ~/cpp/ethereum_bitcoin_wallet_bruteforcer/win.txt ; echo --------------- ; "  &
